4196075c0647a2777d6940b2a65df88372cb77ca
Eliam Enrique Diclo Perez <107940294+Diclodev15@users.noreply.github.com>
2026-02-11 15:24:35 -0400

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Pepe El Grillo");
+       Console.WriteLine("Hola mundo desde C# con GitHub Actions 🚀");
     }
 }
