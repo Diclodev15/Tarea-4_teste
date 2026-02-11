266c444d97b01498009007ef9a8acc38f01263e9
Eliam Enrique Diclo Perez <eliam2k2018@gmail.com>
2026-02-11 15:19:48 -0400

    Modificamos el programa

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,45 +1,9 @@
-﻿int numero1 = 1;
-int numero2 = 2;
-string opcion = "";
+﻿using System;
 
-
-
-Console.WriteLine("Desea ejecutar el programa? (si/no)");
-opcion = Console.ReadLine().ToLower();
-if (opcion == "si")
+class Program
 {
-    Console.Clear();
-    saludar();
-
-}
-else if (opcion == "no")
-{
-    Console.WriteLine("Programa no ejecutado");
-
-}
-else
-{
-    Console.WriteLine("Opcion no valida");
-}
-void saludar()
-{
-
-    Console.WriteLine("Ingrese el primer numero");
-    numero1 = Convert.ToInt32(Console.ReadLine());
-
-    if (numero1 == 1)
-
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hola mundo esta es mi prueba de mi practica #4, de electiva 2 ");
-    }
-    else if (numero2 == 2)
-    {
-        Console.WriteLine("Esta es mi prueba, Hola Mundo");
-    }
-    else
-    {
-        Console.WriteLine("Los numeros no son iguales a 1 y 2");
+        Console.WriteLine("Hola mundo desde C# con GitHub Actions 🚀");
     }
 }
-Console.ReadKey();
-
