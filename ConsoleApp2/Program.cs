359d661e92b80a1b3b7bec8ea6fc78c4994aa0db
Eliam Enrique Diclo Perez <eliam2k2018@gmail.com>
2026-02-11 14:23:52 -0400

    primer commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿int numero1 = 1;
+int numero2 = 2;
+string opcion = "";
+
+
+
+Console.WriteLine("Desea ejecutar el programa? (si/no)");
+opcion = Console.ReadLine().ToLower();
+if (opcion == "si")
+{
+    Console.Clear();
+    saludar();
+
+}
+else if (opcion == "no")
+{
+    Console.WriteLine("Programa no ejecutado");
+
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
+void saludar()
+{
+
+    Console.WriteLine("Ingrese el primer numero");
+    numero1 = Convert.ToInt32(Console.ReadLine());
+
+    if (numero1 == 1)
+
+    {
+        Console.WriteLine("Hola mundo esta es mi prueba de mi practica #4, de electiva 2 ");
+    }
+    else if (numero2 == 2)
+    {
+        Console.WriteLine("Esta es mi prueba, Hola Mundo");
+    }
+    else
+    {
+        Console.WriteLine("Los numeros no son iguales a 1 y 2");
+    }
+}
+Console.ReadKey();
+
