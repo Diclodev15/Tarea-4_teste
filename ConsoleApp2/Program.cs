9071472c0bd31ffe9438d2d318d7968efa79681c
Eliam Enrique Diclo Perez <107940294+Diclodev15@users.noreply.github.com>
2026-02-11 15:23:33 -0400

    Change output message in Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hola mundo desde C# con GitHub Actions 🚀");
+        Console.WriteLine("Pepe El Grillo");
     }
 }
